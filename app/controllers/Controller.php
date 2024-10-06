2660b6051cc1d3df10eaeb134fe467a95f9f59ff
Naiara Maya <irnmaya@lhusurbil.eus>
2024-10-06 10:24:03 +0200

    Mejoras

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -4,17 +4,7 @@ class Controller
 {
     public function __construct()
     {
-        $this->checkSession();
     }
 
-    protected function checkSession()
-    {
-        session_start();
-        if (!isset($_SESSION['user'])) {
-            // Redirect to login page if session is not set
-            header('Location: /index.php?controller=UsuarioController&action=login');
-            exit();
-        }
-    }
 }
 ?>
