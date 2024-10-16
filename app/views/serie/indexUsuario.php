1bb6ce025373524dafb3efbfde6157489114ba07
Naiara Maya <irnmaya@lhusurbil.eus>
2024-10-16 13:28:04 +0200

    Mejora rendimiento base de datos

--- app/views/serie/indexUsuario.php
+++ app/views/serie/indexUsuario.php
@@ -7,8 +7,11 @@
             <td title="<?= $serie->getDescripcion(); ?>"><?php echo $serie->getTitulo(); ?></td>
            <!--  <td><?php echo $serie->getDescripcion(); ?></td> -->
             <td><?php 
-                echo $serie->getPuntuacionUsuario() . ' - ' . $serie->getPuntuacionMedia();
-                $puntuacion = $serie->getPuntuacionUsuario();
+                //echo $serie->getPuntuacionUsuario() . ' - ' . $serie->getPuntuacionMedia();
+                $puntuacion = $serie->getPuntuacionMedia();
+                if ($serie->getPuntuacionUsuario() == null) {
+                    $puntuacion = 0;
+                }
                 for ($i = 1; $i <= floor($puntuacion); $i++) {
                     $id_estrella = 'estrella' . $serie->getId() . '_' . $i;
                     ?>
